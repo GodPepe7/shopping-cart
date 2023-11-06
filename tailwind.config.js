1cbabc94f3dec31472017bf76405b71128afa869
GodPepe7 <privatalexm@gmail.com>
2023-11-06 22:27:36 +0100

    Add custom themes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        home: "url('./src/assets/home-background.png')",
+      },
+      colors: {
+        "theme-white": "#F7F8F7",
+        "theme-black": "#0e110f",
+      },
+    },
   },
   plugins: [],
 };
