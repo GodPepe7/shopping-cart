4d5dbe4b71c7cd347fbd158d2456b82e20b3c810
GodPepe7 <privatalexm@gmail.com>
2023-11-08 22:12:40 +0100

    Implement navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
 import Cart from "../assets/icons/cart.svg";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import CartModal from "./CartModal";
+
+function Header({ color }) {
+  const themes = {
+    white: "theme-white",
+    black: "theme-black",
+  };
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
   return (
     <header
-      className={`sticky flex justify-between items-center p-10 text-2xl font-medium ${styles}`}
+      className={`sticky flex justify-between items-center p-10 text-2xl font-medium text-${themes[color]}`}
     >
       <div>
         <h1>
@@ -13,6 +23,20 @@ import Cart from "../assets/icons/cart.svg";
         </h1>
       </div>
       <nav>
+        <ul className="flex gap-20 list-none items-center">
+          <li
+            className={`hover:bg-${color}/60 bg-opacity-50 px-4 rounded-md hover:cursor-pointer`}
+          >
+            <Link to="/">Home</Link>
+          </li>
+          <li
+            className={`hover:bg-${color}/60 px-4 rounded-md hover:cursor-pointer`}
+          >
+            <Link to="shop/">Shop</Link>
+          </li>
+          <li
+            className={`hover:bg-${color}/60 bg-opacity-25 px-4 rounded-md hover:cursor-pointer`}
+          >
             <button
               className="border-none w-8 h-8"
               onClick={() => setModalIsOpen(true)}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,17 @@
 import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className="min-h-screen bg-home flex flex-col">
-      <Header styles="text-theme-white absolute" />
+      <Header color="white" />
       <div className="text-theme-white max-w-[60ch] self-center my-60 text-center flex flex-col gap-5">
         <h2 className="text-5xl tracking-wider">Your Shop for Manga</h2>
         <p className="text-2xl tracking-wider">
           Discover Manga ranging from old classics to the newest, trending ones!
         </p>
         <button className="px-12 py-2 w-max self-center text-theme-white bg-theme-black shadow-md uppercase tracking-wider shadow-theme-white/50 transition-all ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:border-2">
-          Explore Now
+          <Link to="/shop">Explore Now</Link>
         </button>
       </div>
     </div>
