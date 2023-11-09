b80d90d0a3ea18281d20a5ec7f67dc756bd9c005
GodPepe7 <privatalexm@gmail.com>
2023-11-09 21:02:17 +0100

    Add navigation to details page on click

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 function Card({ id, img, title }) {
   const price = 15.99;
+  const navigate = useNavigate();
+  const handleClick = () => navigate(`${id}`);
+
   return (
-    <div className="bg-white min-w-[225px] max-w-[300px] w-[275px] hover:bg-white/60 hover:scale-105 transition flex-auto px-3 py-3 drop-shadow-md">
+    <div
+      onClick={handleClick}
+      className="bg-white min-w-[225px] max-w-[300px] w-[275px] hover:bg-white/60 hover:scale-105 hover:cursor-pointer transition flex-auto px-3 py-3 drop-shadow-md"
+    >
       <img src={img} alt="" className="w-full h-[400px] object-cover" />
       <h3 className="line-clamp-2">{title}</h3>
       <p>${price}</p>
