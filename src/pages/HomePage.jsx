446f5cb97f1377bd3ef2a5192f080d2234a316d6
GodPepe7 <privatalexm@gmail.com>
2023-11-10 23:40:34 +0100

    Change html semantics for accessibility

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ function HomePage() {
   return (
     <div className="min-h-screen bg-home flex flex-col">
       <Header color="white" />
-      <div className="text-theme-white max-w-[60ch] self-center my-60 text-center flex flex-col gap-5">
+      <main className="text-theme-white max-w-[60ch] self-center my-60 text-center flex flex-col gap-5">
         <h2 className="text-5xl tracking-wider">Your Shop for Manga</h2>
         <p className="text-2xl tracking-wider">
           Discover Manga ranging from old classics to the newest, trending ones!
@@ -13,7 +13,7 @@ function HomePage() {
         <button className="px-12 py-2 w-max self-center text-theme-white bg-theme-black shadow-md uppercase tracking-wider shadow-theme-white/50 transition-all ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:border-2">
           <Link to="/shop">Explore Now</Link>
         </button>
-      </div>
+      </main>
     </div>
   );
 }
