413898c924d9b76d402477a2539f0b6883bfee18
GodPepe7 <privatalexm@gmail.com>
2023-11-10 23:41:57 +0100

    Adjust styling of card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,15 +4,16 @@ function Card({ id, img, title }) {
   const price = 15.99;
   const navigate = useNavigate();
   const handleClick = () => navigate(`${id}`);
-
   return (
     <div
       onClick={handleClick}
-      className="bg-white min-w-[225px] max-w-[300px] w-[275px] hover:bg-white/60 hover:scale-105 hover:cursor-pointer transition flex-auto px-3 py-3 drop-shadow-md"
+      className="bg-white min-w-[200px] max-w-[275px] w-[275px] hover:bg-white/60 hover:scale-105 hover:cursor-pointer transition flex-auto drop-shadow-md"
     >
-      <img src={img} alt="" className="w-full h-[400px] object-cover" />
-      <h3 className="line-clamp-2">{title}</h3>
-      <p>${price}</p>
+      <img src={img} alt="" className="w-full h-[300px] object-cover" />
+      <div className="px-3 py-3">
+        <h3 className="line-clamp-2">{title}</h3>
+        <p>${price}</p>
+      </div>
     </div>
   );
 }
