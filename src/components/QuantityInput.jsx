6d089795bb528f8468598c4a47578f676e9e4327
GodPepe7 <privatalexm@gmail.com>
2023-11-10 23:42:34 +0100

    Remove arrows from number input

--- src/components/QuantityInput.jsx
+++ src/components/QuantityInput.jsx
@@ -1,5 +1,5 @@
 import Plus from "../assets/icons/plus.svg";
-import Minus from "../assets/icons/plus.svg";
+import Minus from "../assets/icons/minus.svg";
 
 function QuantityInput({
   increaseQuantityHandler,
@@ -11,7 +11,13 @@ function QuantityInput({
       <button onClick={decreaseQuantityHandler}>
         <img src={Minus} alt="Decrease Quantity" />
       </button>
-      <input type="number" min={1} max={10} value={quantity} />
+      <input
+        type="number"
+        min={1}
+        max={10}
+        value={quantity}
+        className="text-center [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+      />
       <button onClick={increaseQuantityHandler}>
         <img src={Plus} alt="Increase Quantity" />
       </button>
