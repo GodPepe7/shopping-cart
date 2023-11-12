6c884a461bf4d53f827fcd0e2556e151e136fcd4
GodPepe7 <privatalexm@gmail.com>
2023-11-12 16:15:22 +0100

    Display picture and information in BasketItem

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -1,10 +1,16 @@
-function BasketItem({ img, title, quantity, price }) {
+function BasketItem({ title, coverImage, quantity, price }) {
   return (
-    <div className="grid grid-cols-2">
-      <img src={img} alt="" className="w-full h-auto object-cover" />
-      <div>
-        <h3>{title}</h3>
-        <p>{price * quantity}</p>
+    <div className="grid grid-cols-[4fr_6fr] gap-2 py-3 border-b-[1px] border-theme-black">
+      <img
+        src={coverImage.large}
+        alt="cover img"
+        className="w-full aspect-square object-cover"
+      />
+      <div className="text-lg">
+        <h3 className="line-clamp-2">{title}</h3>
+        <p>Quantity: {quantity}</p>
+        <p>Price: {price}€</p>
+        <p>Sum: {quantity * price}€</p>
       </div>
     </div>
   );
