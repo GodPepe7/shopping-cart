d6d8340bbefde380b7960e619f0f0a94c63ca23d
GodPepe7 <privatalexm@gmail.com>
2023-11-14 18:07:16 +0100

    Remove className duplicate

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ function Button({ children, onClick }) {
   return (
     <button
       onClick={onClick}
-      className="px-12 py-2 w-max self-center text-theme-white bg-theme-black shadow-md uppercase tracking-wider shadow-theme-white/50 transition-all ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:border-2"
+      className="px-12 py-2 w-max self-center text-theme-white bg-theme-black shadow-md uppercase tracking-wider shadow-theme-white/50 transition-all ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:border-2 focus:scale-110"
     >
       {children}
     </button>
