285ae697b05c8672f4ac12786d8d184fa8da4435
GodPepe7 <privatalexm@gmail.com>
2023-11-15 13:16:07 +0100

    Add more margin on x axis for more space on mobile

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ function HomePage() {
   return (
     <div className="min-h-screen bg-home flex flex-col">
       <Header color="white" />
-      <main className="text-theme-white max-w-[60ch] self-center my-60 text-center flex flex-col gap-5">
+      <main className="text-theme-white max-w-[60ch] mx-4 self-center my-60 text-center flex flex-col gap-5">
         <h2 className="text-5xl tracking-wider">Your Shop for Manga</h2>
         <p className="text-2xl tracking-wider">
           Discover Manga ranging from old classics to the newest, trending ones!
